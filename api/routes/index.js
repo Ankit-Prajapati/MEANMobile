683884a72c110b02128df248c5e40be370d7756e
Ankit Prajapati <prajapati.ankit5@yahoo.com>
2017-10-09 05:47:05 -0400

    - Adds route for PUT and DELETE. - Optimize for best performance.

--- api/routes/index.js
+++ api/routes/index.js
@@ -2,17 +2,30 @@ var express = require("express");
 var router = express.Router();
 
 var mobilesController = require("../controllers/mobiles.controllers");
+var reviewsController = require("../controllers/reviews.controllers");
 
 router
     .route("/mobiles")
-    .get(mobilesController.mobilesGetAll);
+    .get(mobilesController.mobilesGetAll)
+    .post(mobilesController.mobilesAddOne);
 
 router
     .route("/mobiles/:mobileId")
-    .get(mobilesController.mobilesGetOne);
+    .get(mobilesController.mobilesGetOne)
+    .put(mobilesController.mobilesUpdateOne)
+    .delete(mobilesController.mobilesDeleteOne);
+
+
+//Review Routes
+router
+    .route("/mobiles/:mobileId/reviews")
+    .get(reviewsController.reviewsGetAll)
+    .post(reviewsController.reviewsAddOne);
 
 router
-    .route("/mobiles/add")
-    .post(mobilesController.mobilesAddOne);
+    .route("/mobiles/:mobileId/reviews/:reviewId")
+    .get(reviewsController.reviewsGetOne)
+    .put(reviewsController.reviewsUpdateOne)
+    .delete(reviewsController.reviewsDeleteOne);
 
 module.exports = router;
