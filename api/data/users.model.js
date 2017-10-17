441a992d639e15bac8b1c81f785c2c4d6f390dc0
Ankit Prajapati <prajapati.ankit5@yahoo.com>
2017-10-17 15:51:42 -0400

    Data Model (Schema) for Users.

---
+++ api/data/users.model.js
@@ -0,0 +1,23 @@
+var mongoose = require("mongoose");
+
+var userSchema = new mongoose.Schema({
+    firstname: {
+        type: String,
+        required: true
+    },
+    lastname: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+mongoose.model("User", userSchema, "users");
