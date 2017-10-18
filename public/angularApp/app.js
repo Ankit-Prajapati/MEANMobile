11757558b4a1b3c2d33209146873a6682f9c5bfd
Ankit Prajapati <prajapati.ankit5@yahoo.com>
2017-10-18 07:04:17 -0400

    - Adds Routes for Profile and Register. - Adds "run" function to Validate inappropriate access to URLs.

--- public/angularApp/app.js
+++ public/angularApp/app.js
@@ -1,35 +1,58 @@
-angular.module("meanMobile", ["ngRoute"])
+angular.module("meanMobile", ["ngRoute", "angular-jwt"])
     .config(config)
+    .run(run)
 
-function config($routeProvider) {
+function config($httpProvider, $routeProvider) {
+
+    $httpProvider.interceptors.push("AuthInterceptor");
 
     $routeProvider
         .when("/", {
+            templateUrl: "/angularApp/home/_home.html",
+            access: {
+                restricted: false
+            }
+        })
+        .when("/mobiles", {
             templateUrl: "/angularApp/mobilesShowcase/_mobilesShowcase.html",
             controller: "mobilesShowcaseController",
-            controllerAs: "vm"
+            controllerAs: "vm",
+            access: {
+                restricted: false
+            }
         })
         .when("/mobiles/:mobileId", {
             templateUrl: "/angularApp/mobileDetails/_mobileDetails.html",
             controller: "mobileDetailsController",
-            controllerAs: "vm"
+            controllerAs: "vm",
+            access: {
+                restricted: false
+            }
         })
-    // .when("/mobiles/:mobileId/reviews", {
-    //     templateUrl: "",
-    //     controller: "",
-    //     controllerAs: ""
-    // })
-    // .when("/mobiles/:mobileId/reviews/:reviewId", {
-    //     templateUrl: "",
-    //     controller: "",
-    //     controllerAs: ""
-    // })
-    // .when("/404", {
-    //     templateUrl: "",
-    //     controller: "",
-    //     controllerAs: ""
-    // })
-    // .otherwise({
-    //     redirectTo: "/404"
-    // })
+        .when("/register", {
+            templateUrl: "/angularApp/register/_register.html",
+            controller: "registerController",
+            controllerAs: "vm",
+            access: {
+                restricted: false
+            }
+        })
+        .when("/profile", {
+            templateUrl: "/angularApp/profile/_profile.html",
+            access: {
+                restricted: true
+            }
+        })
+        .otherwise({
+            redirectTo: "/"
+        })
+}
+
+function run($rootScope, $location, $window, AuthFactory) {
+    $rootScope.$on('$routeChangeStart', function (event, nextRoute, currentRoute) {
+        if (nextRoute.access !== undefined && nextRoute.access.restricted && !$window.sessionStorage.token && !AuthFactory.isLoggedIn) {
+            event.preventDefault();
+            $location.path('/');
+        }
+    });
 }
