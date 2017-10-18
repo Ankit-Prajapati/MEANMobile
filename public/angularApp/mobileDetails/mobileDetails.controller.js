eb9553ab366374c82449c24c13d3f701f7ffec86
Ankit Prajapati <prajapati.ankit5@yahoo.com>
2017-10-18 07:02:49 -0400

    - Adds Review Form. - Add functionality to Post/Add the Review by a Logged In User.

--- public/angularApp/mobileDetails/mobileDetails.controller.js
+++ public/angularApp/mobileDetails/mobileDetails.controller.js
@@ -1,6 +1,6 @@
 angular.module("meanMobile").controller("mobileDetailsController", mobileDetailsController);
 
-function mobileDetailsController($routeParams, apiFactory) {
+function mobileDetailsController($route, $routeParams, $window, apiFactory, AuthFactory, jwtHelper) {
     var vm = this;
     vm.mobileId = $routeParams.mobileId;
 
@@ -12,5 +12,36 @@ function mobileDetailsController($routeParams, apiFactory) {
 
     function _getStarRating(stars) {
         return new Array(stars);
+    }
+
+    vm.isLoggedIn = function () {
+        if (AuthFactory.isLoggedIn) {
+            return true;
+        } else {
+            return false;
+        }
+    };
+
+    vm.addReview = function () {
+
+        var token = jwtHelper.decodeToken($window.sessionStorage.token);
+        var username = token.username;
+
+        var postData = {
+            name: username,
+            rating: vm.rating,
+            review: vm.review
+        };
+        if (vm.reviewForm.$valid) {
+            apiFactory.postReview(id, postData).then(function (response) {
+                if (response.status === 200) {
+                    $route.reload();
+                }
+            }).catch(function (error) {
+                console.log(error);
+            });
+        } else {
+            vm.isSubmitted = true;
+        }
     }
 }
